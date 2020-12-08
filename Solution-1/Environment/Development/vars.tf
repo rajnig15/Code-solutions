0753c6906e3915245881ac24ed261114496df3c7
rajnig15 <48977973+rajnig15@users.noreply.github.com>
2020-12-08 15:50:05 +0530

    Create vars.tf

---
+++ Solution-1/Environment/Development/vars.tf
@@ -0,0 +1,14 @@
+#Variables account_id,role_name & workspace_name can be provided 
+#while executing terraform during run time
+
+variable "account_id" {
+    type = "string"
+}
+
+variable "role_name" {
+   type = "string"
+}
+
+variable "workspace_name" {
+  type = "string"
+}
